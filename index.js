03e838caef3777baafc0dcb83d7f0562e7205c7e
“Malhar <tanksalemalhar123@gmail.com>
2020-10-15 10:31:54 +0530

    First Commit12

--- index.js
+++ index.js
@@ -141,7 +141,7 @@ pool.on('error', function (err, client) {
 app.get('/getinfobyid', function (req, res) {
   // First read existing users.
   var user_name = req.param('name');
-  pool.query(`SELECT * FROM login_tripaxy WHERE name = ${user_name};`, function(err, res) {
+  pool.query(`SELECT * FROM login_tripaxy WHERE name = '${user_name}';`, function(err, res) {
     if(err) {
         return console.error('error running query', err);
     }
