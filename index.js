090a6e4f54d129d1328db6567479f56078a0cbbc
“Malhar <tanksalemalhar123@gmail.com>
2020-10-16 10:54:46 +0530

    First Commit4

--- index.js
+++ index.js
@@ -4,11 +4,7 @@ const app = express();
 const port = 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-let abc = [];
-let name1;
-let name2 = "";
-let name11;
-let name3;
+
 
 var cors = require('cors');
 
@@ -24,10 +20,10 @@ app.options('*', cors());
 
 
 
-//Hello World
+//Welcome Request
 
 app.get('/', (request, response) => {
-  response.send('Welcome to Tripaxy APIs')
+  response.send('Welcome to StarkFlow APIs')
 });
 
 
@@ -50,8 +46,7 @@ pool.on('error', function (err, client) {
 
 
 
-//Get Tasks
-// Display all users
+//Get All Todos
 
 app.get('/getAllTodos', (request, response) => {
   pool.query('SELECT * FROM ToDo', (error, result) => {
@@ -63,7 +58,7 @@ app.get('/getAllTodos', (request, response) => {
 
 
 
-// Add a new user
+// Add a new Todo in the list
 app.post('/addTodo', (request, response) => {
   var id = request.body.id;
   var title = request.body.title;
@@ -71,174 +66,27 @@ app.post('/addTodo', (request, response) => {
   pool.query(`insert into ToDo (ID,title,description) VALUES(${id},'${title}','${description}')`, (error, result) => {
       if (error) throw error;
 
-      response.status(201).send(`User added with ID: ${id}`);
+      response.status(201).send(`Todo added with ID: ${id}`);
   });
 });
 
 
 
-// app.get('/loginn', function (req, res) {
-//   // First read existing users.
-//   pool.query(`SELECT * FROM dashboard WHERE id = '1' ;`, function(err, res) {
-//     if(err) {
-//         return console.error('error running query', err);
-//     }
-
-//     console.log(+JSON.stringify(res.rows[0]));
-//     name1 = JSON.stringify(res.rows[0]);
-    
-// });
-// res.send(name1);
-// })
-
-
-// app.get("/login/:id", function(req, res) { 
-//   // var username = req.body.num1; 
-//   // var num2 = req.body.num2; 
-  
-//   pool.query(`SELECT * FROM login_tripaxy WHERE name = ${req.params.id} ;`, function(err, res) {
-//     if(err) {
-//         return console.error('error running query', err);
-//     }
-
-//     console.log(+JSON.stringify(res.rows[0]));
-//     name11 = JSON.stringify(res.rows[0]);
-   
-// });
-// res.send(name11);
-// })
-
-
-// // Profile Info
-
-// app.get('/profile', function (req, res) {
-//   // First read existing users.
-//   pool.query(`SELECT * FROM login WHERE id = '1' ;`, function(err, res) {
-//     if(err) {
-//         return console.error('error running query', err);
-//     }
-
-//     console.log(+JSON.stringify(res.rows[0]));
-//     name2 = JSON.stringify(res.rows[0]);
-    
-// });
-// res.send(name2);
-// })
 
 
 
-// // Users
+//Update Todo By Id 
 
-// app.get('/users', function (req, res) {
-//   // First read existing users.
-//   pool.query(`SELECT * FROM patientinfo WHERE statusofvisit = 'p';`, function(err, res) {
-//     if(err) {
-//         return console.error('error running query', err);
-//     }
+app.post('/updateTodo', (request, response) => {
+  var id = request.body.id;
+  var title = request.body.title;
+  var description = request.body.description;
+  pool.query(`update ToDo set title = '${title}', description = '${description}' where id = ${id};`, (error, result) => {
+      if (error) throw error;
 
-//     console.log(+JSON.stringify(res.rows));
-//     name2 = JSON.stringify(res.rows);
-    
-// });
-// res.send(name2);
-// })
-
-
-
-
-// // Completed Users
-
-// app.get('/completedUsers', function (req, res) {  
-//   // First read existing users.
-//   pool.query(`SELECT * FROM patientinfo WHERE statusofvisit = 'd';`, function(err, res) {
-//     if(err) {
-//         return console.error('error running query', err);
-//     }
-
-//     console.log(+JSON.stringify(res.rows));
-//     name2 = JSON.stringify(res.rows);
-    
-// });
-// res.send(name2);
-// })
-
-
-
-
-
-
-
-// //Update Status of Visit By Id
-
-// app.get('/updateStatusById/:id', function (req, res) {
-//   // First read existing users.
-//   console.log("inside")
-//   pool.query(`select updateVisitFlag(${req.params.id});`, function(err, res) {
-//     if(err) {
-//         return console.error('error running query', err);
-//     }
-
-   
-//     //name2 = JSON.stringify(res.rows);
-    
-// });
-// res.send("Done")
-// })
-
-
-
-
-// //Get By ID
-
-// app.get('/:id', function (req, res) {
-//   // First read existing users.
-//   pool.query(`SELECT name FROM products WHERE id = ${req.params.id}`, function(err, res) {
-//     if(err) {
-//         return console.error('error running query', err);
-//     }
-
-//     console.log("New Output"+res.rows[0].name);
-//     name1 = res.rows[0].name;
-    
-// });
-// res.send("Name is: "+name1);
-// })
-
-
-// //Add User
-
-// app.get('/api/v1/addUser/', function (req, res) {
-//   pool.query(`INSERT INTO products (product_no, name,price) VALUES ('30', 'Ranjana','10')`, function(err, res) {
-//     if(err) {
-//         return console.error('error running query', err);
-//     }
-//     console.log('Output:', res);
-//    //  abc = res.rows[0].name;
- 
-// });
-// if(res){
-//   res.send("Added Record In Db");
-// }
-
-// })
-
-
-// //Update 
-
-
-// app.get('/api/v1/updateUser/:id', function (req, res) {
-//   pool.query(`UPDATE products SET name='Anant' WHERE id = ${req.params.id}`, function(err, res) {
-//     if(err) {
-//         return console.error('error running query', err);
-//     }
-//     console.log('Output:', res);
-//    //  abc = res.rows[0].name;
-//   });
-// if(res){
-//   res.send("Updated Record In Db");
-// }
-
-// })
+      response.status(201).send(`Todo updated with ID: ${id}`);
+  });
+});
 
 
 
@@ -250,21 +98,3 @@ app.post('/addTodo', (request, response) => {
 app.listen(process.env.PORT || 3000, function() {
     console.log('server running on port 3000', '');
 });
-
-// const express = require('express');
-// const app = express();
-// var cors = require('cors');
-
-// app.use(
-//     cors({
-//         credentials: true,
-//         origin: true
-//     })
-// );
-// app.options('*', cors());
-
-// app.get('/', (req, res) => res.send('Working!!!'));
-
-// app.listen(process.env.PORT || 3000, function() {
-//     console.log('server running on port 3000', '');
-//});
